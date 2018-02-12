c008b3c250616207c3c311c9e7a4b29005b9c138
Kilian Koeltzsch <me@kilian.io>
2018-02-12 14:39:57 +0100

    add RZLogin.__repr__

--- htwdresden/login.py
+++ htwdresden/login.py
@@ -2,3 +2,6 @@ class RZLogin:
     def __init__(self, s_number: str, password: str):
         self.s_number = s_number
         self.password = password
+
+    def __repr__(self):
+        return f'RZLogin <{self.s_number}>'
