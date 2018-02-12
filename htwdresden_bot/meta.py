0a54e53e8177860b9da8a6caf48da6b9fbfb31b2
Kilian Koeltzsch <me@kilian.io>
2018-02-12 15:37:03 +0100

    remove unecessary comma

--- htwdresden_bot/meta.py
+++ htwdresden_bot/meta.py
@@ -4,7 +4,7 @@ from telegram.parsemode import ParseMode
 
 def _start_cmd(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
-                     text=f'Hi, {update.message.chat.first_name} 👋,\ntippe `/` um Vorschläge für meine möglichen '
+                     text=f'Hi {update.message.chat.first_name} 👋,\ntippe `/` um Vorschläge für meine möglichen '
                           f'Aktionen zu sehen.',
                      parse_mode=ParseMode.MARKDOWN)
 
