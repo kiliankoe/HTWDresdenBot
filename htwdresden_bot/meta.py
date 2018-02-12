e020e8e3e2201217bd00a29e79b1bc28372ced48
Kilian Koeltzsch <me@kilian.io>
2018-02-12 14:33:33 +0100

    use markdown parsemode for start handler

--- htwdresden_bot/meta.py
+++ htwdresden_bot/meta.py
@@ -1,9 +1,12 @@
 from telegram.ext import CommandHandler
+from telegram.parsemode import ParseMode
 
 
 def _start_cmd(bot, update):
     bot.send_message(chat_id=update.message.chat_id,
-                     text='Hallo, ich bin der HTW Bot. Tippe `/` um Vorschläge für meine möglichen Aktionen zu sehen.')
+                     text=f'Hi, {update.message.chat.first_name} 👋,\ntippe `/` um Vorschläge für meine möglichen '
+                          f'Aktionen zu sehen.',
+                     parse_mode=ParseMode.MARKDOWN)
 
 
 start_handler = CommandHandler('start', _start_cmd)
