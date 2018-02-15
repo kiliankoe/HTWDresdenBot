2c2403562cecfaba0a1041cb2b66e457b4480c8d
Kilian Koeltzsch <me@kilian.io>
2018-02-15 22:07:45 +0100

    implement logging facilities
    also remove maintainer message on startup and reorder main.py somewhat

--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 import os
+import logging
+import argparse
 from telegram.ext import Updater
 from telegram.error import TelegramError, Unauthorized
 from htwdresden_bot import *
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--log', dest='loglevel', default='ERROR', help='log level')
+args = parser.parse_args()
+
+numeric_level = getattr(logging, args.loglevel.upper(), None)
+if not isinstance(numeric_level, int):
+    raise ValueError('Invalid log level: {}'.format(args.loglevel))
+logging.basicConfig(level=numeric_level)
+
 
 GRADE_NOTIFICATION_INTERVAL = 3_600
-
-
-def on_startup(bot, _):
-    maintainer_chat_id = os.getenv('MAINTAINER_CHAT_ID')
-    if maintainer_chat_id is not None:
-        bot.send_message(chat_id=maintainer_chat_id, text='Running...')
-
-
-def error_handler(bot, update, error):
-    try:
-        raise error
-    except Unauthorized:
-        # user has blocked this bot, no need to update grades anymore
-        db.remove_login(update.message.chat_id)
-    except TelegramError:
-        pass
-
 
 db.setup()
 
 updater = Updater(token=os.getenv('TELEGRAM_BOT_TOKEN'))
-updater.job_queue.run_once(on_startup, 0)
 
 dispatcher = updater.dispatcher
 
@@ -38,8 +31,21 @@ dispatcher.add_handler(free_rooms_handler)
 dispatcher.add_handler(meals_handler)
 dispatcher.add_handler(meal_search_handler)
 
-dispatcher.add_error_handler(error_handler)
+logging.info('Polling for new messages...')
+updater.start_polling()
 
 updater.job_queue.run_repeating(notify_grades, interval=GRADE_NOTIFICATION_INTERVAL, first=0)
 
-updater.start_polling()
+
+def error_handler(bot, update, error):
+    try:
+        raise error
+    except Unauthorized:
+        # user has blocked this bot, no need to update grades anymore
+        db.remove_login(update.message.chat_id)
+    except TelegramError as e:
+        logging.error(f'unexpected telegram error {e}')
+        pass
+
+
+dispatcher.add_error_handler(error_handler)
